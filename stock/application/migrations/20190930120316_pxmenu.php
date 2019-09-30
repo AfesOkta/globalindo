51d5217e781175596512a647a838556b4cd8c845
revisoft783 <revisoft783@gmail.com>
2019-09-30 12:28:58 +0700

    add migrations table

---
+++ stock/application/migrations/20190930120316_pxmenu.php
@@ -0,0 +1,63 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+class Migration_Pxmenu extends CI_Migration
+{
+    public function up()
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        // Drop table 'table_name' if it exists
+        $this->dbforge->drop_table('pxmenu', true);
+
+        // Table structure for table 'table_name'
+        $this->dbforge->add_field(array(
+            'id' => array(
+                'type' => 'MEDIUMINT',
+                'constraint' => '8',
+                'unsigned' => true,
+                'auto_increment' => true
+            ),
+            'title' => array(
+                'type' => 'varchar',
+                'constraint' => '255',
+                'null' => true,
+            ),
+            'url' => array(
+                'type' => 'varchar', 
+                'constraint' => '255',
+                'null' => true,
+            ),
+            'icon' => array(
+                'type' => 'varchar', 
+                'constraint' => '255',
+                'null' => true,
+            ),
+            'ismain_menu' => array(
+                'type' => 'integer', 
+                'constraint' => '11',
+                'null' => true,
+            ), 
+            'isaktif'=> array(
+                'type' => 'Enum', 
+                'constraint' => '255',
+                'null' => true,
+            ), 
+            'position' => array(
+                'type' => 'integer', 
+                'constraint' => '11',
+                'null' => true,
+            ),
+            'created_at' => array(
+                'type' => 'DATETIME',
+                'null' => false,
+            )
+        ));
+        $this->dbforge->add_key('id', true);
+        $this->dbforge->create_table('pxmenu');
+    }
+
+    public function down()
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->dbforge->drop_table('pxmenu', true);
+    }
+}
