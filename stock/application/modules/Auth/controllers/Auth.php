d4af788e5df7a81c91bdda0479f3d4b27053be2f
revisoft783 <revisoft783@gmail.com>
2019-10-07 01:00:27 +0700

    add style metronic

--- stock/application/modules/Auth/controllers/Auth.php
+++ stock/application/modules/Auth/controllers/Auth.php
@@ -24,8 +24,32 @@ class Auth extends MY_Controller {
     function index() {
         $data['assets'] = [
             'bootstrap_css' => base_url('assets/css/bootstrap.min.css'),
+            'bootstrap_responsife_css' => base_url('assets/css/bootstrap_responsive.min.css'),
+            'fontawesome_css' => base_url('assets/plugins/font-awesome/css/font-awesome.css'),
+            'style_metro' => base_url('assets/css/style-metro.css'),
+	        'style' => base_url('assets/css/style.css'),
+            'style_responsive' => base_url('assets/css/style-responsive.css'),
+            'default' => base_url('assets/css/themes/default.css'),
+	        'uniform_style' => base_url('assets/plugins/uniform/css/uniform.default.css'),
+	        'select2_metro' => base_url('assets/plugins/select2/select2_metro.css'),
+	        'login_soft'=> base_url('assets/css/pages/login-soft.css'),
             'bootstrap_js' => base_url('assets/js/bootstrap.min.js'),
             'jquery' => base_url('assets/js/jquery-2.2.4.min.js'),
+            'jquery_migrate' => base_url('assets/plugins/jquery-migrate-1.2.1.min.js'),	
+	        'jquery_ui' =>base_url('assets/plugins/jquery-ui/jquery-ui-1.10.1.custom.min.js'),
+	        'twitter_bootstrap_hover_dropdown' => base_url('assets/plugins/bootstrap-hover-dropdown/twitter-bootstrap-hover-dropdown.min.js'),
+	        'excanvas' =>base_url('assets/plugins/excanvas.min.js'),
+	        'respond'=> base_url('assets/plugins/respond.min.js'), 
+	        'jquery_slimscroll' => base_url('assets/plugins/jquery-slimscroll/jquery.slimscroll.min.js'),
+	        'jquery_blockui' => base_url('assets/plugins/jquery.blockui.min.js'),
+	        'jquery_cookie' => base_url('assets/plugins/jquery.cookie.min.js'),
+	        'jquery_uniform' => base_url('assets/plugins/uniform/jquery.uniform.min.js'),        
+            'jquery_validation' => base_url('assets/plugins/jquery-validation/dist/jquery.validate.min.js'),
+	        'jquery_backstretch' => base_url('assets/plugins/backstretch/jquery.backstretch.min.js'),
+	        'select2' => base_url('assets/plugins/select2/select2.min.js'),
+    	    'app' => base_url('assets/scripts/app.js'),
+	        'login_soft' => base_url('assets/scripts/login-soft.js'),    
+	
         ];
         $this->load->view('login', $data);
     }
--- stock/application/modules/Auth/views/login.php
+++ stock/application/modules/Auth/views/login.php
@@ -9,7 +9,16 @@ defined('BASEPATH') or exit('No direct script access allowed');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Migrate Table</title>
-    <link rel="stylesheet" href="<?= $assets['bootstrap_css'] ?>">
+    <link rel="stylesheet" href="<?= $assets['bootstrap_css'] ?>">    
+	<link href="<?= $assets['bootstrap_responsive'] ?>"/>
+	<link href="<?= $assets['fontawesome']?>"/>
+	<link href="<?= $assets['style_metro']?>"/>
+	<link href="<?= $assets['style.css'] ?>"/>
+	<link href="<?= $assets['style_responsive.css']?>"/>
+	<link href="<?= $assets['themes/default.css']?>"/>
+	<link href="<?= $assets['uniform_style']?>"/>
+	<link href="<?= $assets['select2_metro']?>"< />
+	<link href="<?= $assets['login_soft']?>"/>
     <style>
     body {
         padding-top: 20px;
@@ -17,42 +26,88 @@ defined('BASEPATH') or exit('No direct script access allowed');
     </style>
 </head>
 
-<body>
+<body class="login">
+    <h1 class="text-center">
+        <i class="glyphicon glyphicon-fire"></i>Login<br>
+        <small>An easy way to manage stock</small>
+    </h1>
     <div class="container">
 
-        <div class="row">
-            <div class="col-sm-4">
-
-                <h1 class="text-center">
-                    <i class="glyphicon glyphicon-fire"></i>Login<br>
-                    <small>An easy way to manage stock</small>
-                </h1>
-
-                <form class="form-signin">
-                    <div class="form-label-group">
-                        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required
-                            autofocus>
-                        <label for="inputEmail">Email address</label>
-                    </div>
-
-                    <div class="form-label-group">
-                        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-                        <label for="inputPassword">Password</label>
-                    </div>
-
-                    <div class="custom-control custom-checkbox mb-3">
-                        <input type="checkbox" class="custom-control-input" id="customCheck1">
-                        <label class="custom-control-label" for="customCheck1">Remember password</label>
-                    </div>
-                    <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Sign
-                        in</button>
-                </form>
-            </div>
+        <div class="content">
+            <!-- BEGIN LOGIN FORM -->
+		<form class="form-vertical login-form" action="index.html" method="post">
+			<h3 class="form-title">Login to your account</h3>
+			<div class="alert alert-error hide">
+				<button class="close" data-dismiss="alert"></button>
+				<span>Enter any username and password.</span>
+			</div>
+			<div class="control-group">
+				<!--ie8, ie9 does not support html5 placeholder, so we just show field title for that-->
+				<label class="control-label visible-ie8 visible-ie9">Username</label>
+				<div class="controls">
+					<div class="input-icon left">
+						<i class="icon-user"></i>
+						<input class="m-wrap placeholder-no-fix" type="text" autocomplete="off" placeholder="Username" name="username"/>
+					</div>
+				</div>
+			</div>
+			<div class="control-group">
+				<label class="control-label visible-ie8 visible-ie9">Password</label>
+				<div class="controls">
+					<div class="input-icon left">
+						<i class="icon-lock"></i>
+						<input class="m-wrap placeholder-no-fix" type="password" autocomplete="off" placeholder="Password" name="password"/>
+					</div>
+				</div>
+			</div>
+			<div class="form-actions">
+				<label class="checkbox">
+				<input type="checkbox" name="remember" value="1"/> Remember me
+				</label>
+				<button type="submit" class="btn blue pull-right">
+				Login <i class="m-icon-swapright m-icon-white"></i>
+				</button>            
+			</div>
+			<div class="forget-password">
+				<h4>Forgot your password ?</h4>
+				<p>
+					no worries, click <a href="javascript:;"  id="forget-password">here</a>
+					to reset your password.
+				</p>
+			</div>
+			<div class="create-account">
+				<p>
+					Don't have an account yet ?&nbsp; 
+					<a href="javascript:;" id="register-btn" >Create an account</a>
+				</p>
+			</div>
+		</form>
         </div>
 
         <script src="<?= $assets['jquery'] ?>"></script>
         <script src="<?= $assets['bootstrap_js'] ?>"></script>
+        <script src="<?= $assets['jquery_migrate'] ?>"></script>
+        <script src="<?= $assets['jquery_ui'] ?> "></script>      
+        <script src="<?= $assets['twitter_bootstrap_hover_dropdown'] ?>"></script>
+        <script src="<?= $assets['jquery_slimscroll'] ?> "></script>
+        <script src="<?= $assets['jquery_blockui'] ?> "></script>
+        <script src="<?= $assets['jquery_cookie']?>"></script>
+        <script src="<?= $assets['jquery_uniform.min'] ?>"></script>
+        <script src="<?= $assets['jquery-validation/dist/jquery.validate.min.js']?>"></script>
+        <script src="<?= $assets['jquery.backstretch.min.js'] ?>"></script></script>
+        <script src="<?= $assets['select2']?>"></script>
+        <script src="<?= $assets['scripts/app.js']?>"></script>
+        <script src="<?= $assets['scripts/login-soft.js'] ?>"></script
+        
         <script>
+            jQuery(document).ready(function() {     
+            App.init();
+            Login.init();
+            });
+        </script>
+
+        <script>
+                
         function msg(parent, type, r) {
             var h = '';
             if (r.header) {
